a11ff8b66181a5c7235637896e2dac6459088972
VampireLover99 <sophia.garbin.santos@escola.pr.gov.br>
2025-10-16 11:34:16 -0300

    Update animation.java

--- animation.java
+++ animation.java
@@ -1,9 +1,1 @@
-// JavaScript
-window.addEventListener('scroll', () => {
-  const alunos = document.querySelectorAll('.animar');
-  alunos.forEach(aluno => {
-    if (aluno.getBoundingClientRect().top < window.innerHeight) {
-      aluno.classList.add('visivel');
-    }
-  });
-});
+
